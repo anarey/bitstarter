9286501d607a4fa652e0867c5b54e3611998c0d1
Ana Rey <anarey@gmail.com>
2013-07-14 10:47:25 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = new Buffer();
+  buffer.write(fs.readFileSync('index.html'), "utf-8");
+  response.send(buffer.toString);
+//  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
