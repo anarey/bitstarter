7faab0ecdbbdc7c2f1daff7e2b95b5f26c1001c2
Ana Rey <anarey@gmail.com>
2013-07-14 11:44:10 +0000

    Updated web.js with sendfile()

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(Array());
-  buffer.write(fs.readFileSync('index.html', "utf-8"));
-  response.send(buffer.toString('utf-8'));
+//  var buffer = new Buffer(Array());
+//  buffer.write(fs.readFileSync('index.html', 'utf-8'));
+//  response.send(buffer.toString('utf-8'));
 //  response.send('Hello World 2!');
+  response.sendfile('index.html')
 });
 
 var port = process.env.PORT || 5000;
