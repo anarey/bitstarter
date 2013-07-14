ac563f0ae72b66d9eb2ffb7ea3a4ba7ba2ae9b41
Ana Rey <anarey@gmail.com>
2013-07-14 11:37:23 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(Array[]);
+  var buffer = new Buffer(Array());
   buffer.write(fs.readFileSync('index.html', "utf-8"));
   response.send(buffer.toString('utf-8'));
 //  response.send('Hello World 2!');
